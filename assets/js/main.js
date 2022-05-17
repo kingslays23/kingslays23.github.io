05120eabdab3a77adeac870136b8ad33017bdb98
kingslay <i.kkingslays@gmail.com>
2022-05-17 13:12:55 +0100

    2nd iteration

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,1 @@
-const navMenu = document.getElementById('nav-menu'),
-       navToggle = document.getElementById('nav-Toggle')  
-       navClose = document.getElementById('nav-close') 
 
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show_menu')
-    })
-}
-
-if(navClose){
-    navClose.addEventListener('click',() =>{
-        navMenu.classList.remove('show_menu')
-    })
-}
